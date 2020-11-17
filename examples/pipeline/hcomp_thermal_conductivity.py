c013d13f3ea9dd17f393df1cb59d55259f6e4e09
Jacob Sonne <jacob.sonne@nov.com>
2020-11-17 11:10:33 +0100

    clean up

--- examples/pipeline/hcomp_thermal_conductivity.py
+++ examples/pipeline/hcomp_thermal_conductivity.py
@@ -3,7 +3,5 @@ ks = {'pvdf': 0.23, 'xlpe': 0.3, 'hdpe': 0.22, 'pa11': 0.24}
 
 def thermal_conductivity(_input_consumed, _results_consumed, results_provided):
     sleep(2.)
-    # print("thermal_conductivity", "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX")
-    # print _input_consumed, ks[ _input_consumed["material"] ]
     results_provided["k_therm"] = ks[ _input_consumed["material"] ]
 

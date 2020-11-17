8cc31b846c038cf9e32e1ea12d1ed53a0b12e046
Jacob Sonne <jacob.sonne@nov.com>
2020-11-17 10:32:05 +0100

    pep8

--- examples/pipeline/hcomp_radial_thermal_profile.py
+++ examples/pipeline/hcomp_radial_thermal_profile.py
@@ -10,8 +10,8 @@ def radial_thermal_prof(_input_consumed, _results_consumed, results_provided):
     tb = _input_consumed["temp_bore"]
     to = _input_consumed["temp_out"]
     time.sleep(0.25)
-    results_provided["temperatures"] = [1,2,3]
-    results_provided["heat_flows"] = [4,5,6]
+    results_provided["temperatures"] = [1, 2, 3]
+    results_provided["heat_flows"] = [4, 5, 6]
 
 def version():
     return 1.0

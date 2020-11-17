97626379f72b745968f7ff8ff83ea9666ab064d4
Jacob Sonne <jacob.sonne@nov.com>
2020-11-17 10:35:32 +0100

    fix typo in model

--- examples/pipeline/hcomp_thermal_conductivity.py
+++ examples/pipeline/hcomp_thermal_conductivity.py
@@ -3,9 +3,7 @@ ks = {'pvdf': 0.23, 'xlpe': 0.3, 'hdpe': 0.22, 'pa11': 0.24}
 
 def thermal_conductivity(_input_consumed, _results_consumed, results_provided):
     sleep(2.)
-    print("thermal_conductivity", "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX")
-    print _input_consumed
-#    results_provided["ktherm"] = [ks[_input_consumed["materials"]] ]
-    print [ks[material] for material in _input_consumed["materials"]]
-    results_provided["ks_walls"] = [ks[material] for material in _input_consumed["materials"]]
+    # print("thermal_conductivity", "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX")
+    # print _input_consumed, ks[ _input_consumed["material"] ]
+    results_provided["k_therm"] = ks[ _input_consumed["material"] ]
 

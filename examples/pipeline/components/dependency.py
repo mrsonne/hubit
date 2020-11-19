9a2885ce05626c23c3ea24914da0942dc1ceb1dd
Jacob Sonne <jacob.sonne@nov.com>
2020-11-19 20:00:53 +0100

    delete components

--- examples/pipeline/components/dependency.py
+++
@@ -1,9 +0,0 @@
-
-class CalculationClass(object):
-
-	def __init__(self):
-		self._prices = {'pvdf': 10, 'xlpe': 1.5, 'hdpe': 1, 'pa11': 7}
-
-	
-	def price_seg(self, mats, ts, ods, length):
-		return length*sum([self._prices[mat]*t*3.14*od for mat, t, od in zip(mats, ts, ods)])

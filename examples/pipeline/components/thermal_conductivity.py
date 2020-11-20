9418d13ca6e1b6c95b6edb5466274b94760a3ef0
Jacob Sonne <jacob.sonne@nov.com>
2020-11-20 05:55:41 +0100

    change example from pipeline to a wall

--- examples/pipeline/components/thermal_conductivity.py
+++ examples/pipeline/components/thermal_conductivity.py
@@ -1,5 +1,11 @@
 from time import sleep
-ks = {'pvdf': 0.23, 'xlpe': 0.3, 'hdpe': 0.22, 'pa11': 0.24}
+# thermal conductivities in W/m/K
+ks = {'brick': 0.47,
+      'concrete': 1.1,
+      'air': 0.025,
+      'styrofoam': 0.04,
+      'glass_wool': 0.030,
+      'rock_wool': 0.032}
 
 def thermal_conductivity(_input_consumed, _results_consumed, results_provided):
     sleep(2.)

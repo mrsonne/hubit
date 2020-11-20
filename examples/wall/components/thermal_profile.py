c0ead93c067a746d784be7d68c9462ead2dda35d
Jacob Sonne <jacob.sonne@nov.com>
2020-11-20 14:59:40 +0100

    add thickness subscription to thermal profile

--- examples/wall/components/thermal_profile.py
+++ examples/wall/components/thermal_profile.py
@@ -7,8 +7,10 @@ def thermal_prof(_input_consumed, _results_consumed, results_provided):
     # print 'workerfun_thermal:', p.name, p.pid
     # ks = _results_consumed["ktherm"]
     ks = _results_consumed["ks_walls"]
-    tb = _input_consumed["temp_in"]
-    to = _input_consumed["temp_out"]
+    t_in = _input_consumed["temp_in"]
+    t_out = _input_consumed["temp_out"]
+    thicknesses = _input_consumed["thicknesses"]
+    print('thicknesses thicknesses thicknesses thicknesses', thicknesses)
     time.sleep(0.25)
     results_provided["temperatures_all_layers"] = [1, 2, 3]
     results_provided["heat_flows_all_layers"] = [4, 5, 6]

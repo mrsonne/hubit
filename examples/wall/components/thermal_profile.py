3ba0a99000392c1f1acfdfc211e2c73edf202e79
Jacob Sonne <jacob.sonne@nov.com>
2020-11-23 11:35:50 +0100

    improve  sign notation

--- examples/wall/components/thermal_profile.py
+++ examples/wall/components/thermal_profile.py
@@ -10,25 +10,26 @@ def thermal_prof(_input_consumed, _results_consumed, results_provided):
     time.sleep(0.25)
 
     # Get data consumed
+    # thickness = x2 - x1 > 0
     thicknesses = _input_consumed["thicknesses"]
     ks = _results_consumed["ks_walls"]
     T_in = _input_consumed["temp_in"]
     T_out = _input_consumed["temp_out"]
 
-    # Calculate total thermal resitance
+    # Calculate total thermal resistance
     Rs = [t/k for (k, t) in zip(ks, thicknesses)]
     R_tot = sum(Rs)
 
     # Calculate heat flux [J/s/m^2]
-    q = ( T_out - T_in ) / R_tot
+    q = - ( T_out - T_in ) / R_tot
 
-    # Calculate temperature drops
-    temp_drops = [ q*R for R in Rs ]
+    # Calculate temperature changes in the positive direction 
+    temp_changes = [ -q*R for R in Rs ]
 
     # Calculate temperature on the outside of the layers
-    T_layer_outer =  [T_in + temp_drops[0]]
-    for temp_drop in temp_drops[1:]:
-        T_layer_outer.append(T_layer_outer[-1] + temp_drop)
+    T_layer_outer =  [T_in + temp_changes[0]]
+    for temp_change in temp_changes[1:]:
+        T_layer_outer.append(T_layer_outer[-1] + temp_change)
 
     results_provided["outer_temperature_all_layers"] = T_layer_outer
     results_provided["heat_flux"] = q

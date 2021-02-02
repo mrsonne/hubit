febe0545dc3560b0033f3d8617f18eb76c6601f0
Jacob Sonne <mrsonne@gmail.com>
2021-02-02 06:27:19 +0100

    make example non-rectangular!!

--- examples/wall/components/thermal_conductivity.py
+++ examples/wall/components/thermal_conductivity.py
@@ -4,8 +4,8 @@ ks = {'brick': 0.47,
       'concrete': 1.1,
       'air': 0.025,
       'styrofoam': 0.04,
-      'glass_wool': 0.030,
-      'rock_wool': 0.032}
+      'glasswool': 0.030,
+      'rockwool': 0.032}
 
 def thermal_conductivity(_input_consumed, _results_consumed, results_provided):
       """Use 'material' in the input to compute the corresponding thermal

48295ea89703887c4b02f788cf4f38809b97526a
Jacob Sonne <mrsonne@gmail.com>
2021-02-07 21:22:27 +0100

    improve sweep

--- examples/wall/run_sweep.py
+++ examples/wall/run_sweep.py
@@ -1,6 +1,8 @@
 import logging
+from operator import getitem
 from typing import Any
 from .utils import get_model, HubitModel
+from hubit.shared import convert_to_internal_path
 logging.basicConfig(level=logging.INFO)
 
 def make_sweep(hmodel: HubitModel, nproc:Any=None) -> None:
@@ -15,21 +17,27 @@ def make_sweep(hmodel: HubitModel, nproc:Any=None) -> None:
 
     # For segment 0 sweep over multiple inputs created as the 
     # Cartesian product of the input perturbations 
-    input_values_for_path = {"segments[0].layers[0].material": ('brick', 'concrete'),
-                             "segments[0].layers[0].thickness": (0.08, 0.12, 0.15, 0.46,),
+    input_values_for_path = {"segments[0].layers[2].material": ('rockwool', 'glasswool'),
+                             "segments[0].layers[2].thickness": (0.05, 0.15, 0.25, 0.35),
+                             "segments[1].layers[1].thickness": (0.025, 0.04, 0.055),
                             }
 
-
+    input_paths = list(input_values_for_path.keys())
     responses, inps, _ = hmodel.get_many(queries,
                                          input_values_for_path,
                                          nproc=nproc)
-    for inp, response in zip(inps, responses):
-        print('Input',
-              inp["segments.0.layers.0.material"],
-              inp["segments.0.layers.0.thickness"])
-        print('Response', response)
-        print('')
 
+    # Print results in a primitive table
+    headers = input_paths + list(responses[0].keys())
+    fstr = len(headers)*'{:<32} '
+    lines = [fstr.format(*headers)]
+    for inp, response in zip(inps, responses):
+        values = [getitem(inp, convert_to_internal_path(ipath)) 
+                  for ipath in input_paths]
+        values.extend( [response[qpath] for qpath in queries] )
+        lines.append( fstr.format(*values) )
+
+    print('\n'.join(lines))
 
 if __name__ == '__main__': # Main guard required on windows if mpworkers = True
     make_sweep(get_model(render=False), nproc=None)

a97b1b948a628778193f442ad91253c9e4807fa7
Jacob Sonne <mrsonne@gmail.com>
2021-02-09 23:05:14 +0100

    adjust prices and densities

--- examples/wall/components/segment_cost.py
+++ examples/wall/components/segment_cost.py
@@ -1,13 +1,16 @@
 import time
 
-# USD / kg
-prices = {'brick': 2000.,
-          'concrete': 2400.,
-          'air': 0.,
-          'styrofoam': 25.,
-          'glasswool': 16.,
-          'rockwool': 40.}
+KG_PER_LBS = 0.453592
 
+# USD / kg
+prices = {'brick': 1.1 / (4.6*KG_PER_LBS), # https://www.homedepot.com/p/7-5-8-in-x-2-1-4-in-x-3-5-8-in-Clay-Brick-20050941/100676108?MERCH=REC-_-pip_alternatives-_-100323015-_-100676108-_-N 
+          'concrete': 1.7 / (33*KG_PER_LBS), #https://www.homedepot.com/p/16-in-x-8-in-x-4-in-Normal-Weight-Concrete-Block-Solid-H0408160003000000/312064973?MERCH=REC-_-pip_alternatives-_-202535931-_-312064973-_-N
+          'air': 0.,
+          'styrofoam': 8.45 / 6. / (2.*KG_PER_LBS) , #https://www.homedepot.com/p/3-4-in-x-1-25-ft-x-4-ft-R-2-65-Polystyrene-Panel-Insulation-Sheathing-6-Pack-150705/202090272
+          'glasswool': 35. / (17.1 * KG_PER_LBS) ,
+          'rockwool': 621. / 12. / (36.8 * KG_PER_LBS) # https://www.homedepot.com/p/ROCKWOOL-R-30-ComfortBatt-Fire-Resistant-Mineral-Wool-Insulation-Batt-15-in-x-47-in-12-bag-RXCB301525/205972559
+        }
+        
 def cost(_input_consumed, _results_consumed, results_provided):
     """
     """
--- examples/wall/components/weight.py
+++ examples/wall/components/weight.py
@@ -1,10 +1,10 @@
 # density in kg/m^3
 densities = {'brick': 2000.,
-             'concrete': 2400.,
+             'concrete': 1835., 
              'air': 0.,
              'styrofoam': 25.,
-             'glasswool': 16.,
-             'rockwool': 40.}
+             'glasswool': 11.,
+             'rockwool': 60.}
 
 
 def weight(_input_consumed, _results_consumed, results_provided):

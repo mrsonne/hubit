48873ba4f2c9e1abd158c50449eb0e381587b12c
Jacob Sonne <mrsonne@gmail.com>
2021-02-10 20:06:25 +0100

    remove render flag

--- examples/wall/run_set_results.py
+++ examples/wall/run_set_results.py
@@ -41,6 +41,6 @@ def query_with_custom_results(hmodel: HubitModel, mpworkers=False) -> None:
 
 
 if __name__ == '__main__': # Main guard required on windows if mpworkers = True
-    hmodel = get_model(render=False)
+    hmodel = get_model()
     use_multiprocessing = True
     query_with_custom_results(hmodel, mpworkers=use_multiprocessing)

3bbe744de34fae91102ea02d2535384c3a1e0a42
Jacob Sonne <mrsonne@gmail.com>
2021-02-10 20:20:41 +0100

    show timing

--- examples/wall/run_queries.py
+++ examples/wall/run_queries.py
@@ -1,4 +1,5 @@
 import logging
+import time
 from .utils import get_model, HubitModel
 
 logging.basicConfig(level=logging.INFO)
@@ -20,17 +21,20 @@ def termal_query(hmodel: HubitModel, mpworkers: bool=False) -> None:
 
     # Make the queries
     queries = (
-                # ["segments[:].layers[:].weight"],
+                ["segments[:].layers[:].weight"],
                 ["heat_transfer_number", 'energy_class', 'total_cost'], 
-                # ["segments[:].heat_flow"], 
-                # ["segments[:].layers[:].outer_temperature"], 
-                # ["segments[0].layers[0].outer_temperature"],   
-                # ["segments[:].layers[1].k_therm"], 
-                # ["segments[0].layers[0].k_therm"],
-                # ["segments[0].layers[:].k_therm"], 
-                # ["segments[:].layers[0].k_therm"],
-                # ["segments[:].layers[:].k_therm"],
+                ["heat_transfer_number"], 
+                ["segments[:].heat_flow"], 
+                ["segments[:].layers[:].outer_temperature"], 
+                ["segments[0].layers[0].outer_temperature"],   
+                ["segments[:].layers[1].k_therm"], 
+                ["segments[0].layers[0].k_therm"],
+                ["segments[0].layers[:].k_therm"], 
+                ["segments[:].layers[0].k_therm"],
+                ["segments[:].layers[:].k_therm"],
               ) 
+
+    time1 = time.time()
 
     # Run queries one by one (slow)
     for query in queries:
@@ -40,6 +44,7 @@ def termal_query(hmodel: HubitModel, mpworkers: bool=False) -> None:
         print(response)
         print('')
 
+    time2 = time.time()
 
     # Run queries as one (fast). The speed increase comes from Hubit's 
     # results caching that acknowleges that the first query actually produces 
@@ -48,7 +53,11 @@ def termal_query(hmodel: HubitModel, mpworkers: bool=False) -> None:
     response = hmodel.get(queries,
                           mpworkers=mpworkers)
     print(response)
-    print('')
+    time3 = time.time()
+
+    print( f'\nSummary' )
+    print( f'Time for separate queries: {time2 - time1:.1f} s' )
+    print( f'Time for joint queries: {time3 - time2:.1f} s' )
 
 
 if __name__ == '__main__': # Main guard required on windows if mpworkers = True

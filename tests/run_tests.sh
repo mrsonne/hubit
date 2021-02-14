cadec2eb794c3156b05c56c40cb70ad9e6c73af8
Jacob Sonne <mrsonne@gmail.com>
2021-02-14 23:02:58 +0100

    add coverage

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -1,4 +1,6 @@
 cd ..
-python -m unittest discover -f -s ./tests -p "*_test.py"
+coverage run --source=. -m unittest discover -s ./tests -p "*_test.py"
+coverage report -m --omit='./tests/*','./examples/*','./docs/*','setup.py' 
+coverage html --omit='./tests/*','./examples/*','./docs/*','setup.py' -d htmlcov 
 cd tests
 read -n 1 -s -r -p "Press any key to continue"

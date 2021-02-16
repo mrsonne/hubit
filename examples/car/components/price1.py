6e84b5cf93a1644108bcfbce2bc44dbaab22b1f4
Jacob Sonne <mrsonne@gmail.com>
2021-02-16 23:42:09 +0100

    delete white space

--- examples/car/components/price1.py
+++ examples/car/components/price1.py
@@ -1,7 +1,7 @@
 from shared import PRICE_FOR_NAME
 
 def price(_input_consumed, _results_consumed, results_provided):
-    counts = _input_consumed['part_counts'] 
+    counts = _input_consumed['part_counts']
     names = _input_consumed['part_names'] 
 
     unit_prices = [PRICE_FOR_NAME[name] for name in names]

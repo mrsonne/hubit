cf6dec432ad3a1f468b7b79c287ca24e2cba2f03
Jacob Sonne <mrsonne@gmail.com>
2021-02-19 20:30:29 +0100

    add versions

--- examples/car/components/price1.py
+++ examples/car/components/price1.py
@@ -10,3 +10,7 @@ def price(_input_consumed, _results_consumed, results_provided):
                     for count, unit_price in zip(counts, unit_prices) ] )
 
     results_provided['car_price'] = result
+
+
+def version():
+    return '1.0.0'

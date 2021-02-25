43a1dd98439d10d8160d7b1cd0802754ed3e89ae
Jacob Sonne <mrsonne@gmail.com>
2021-02-25 21:02:44 +0100

    set the source as the hubit folder

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -1,7 +1,7 @@
 cd ..
-coverage run --source=. -m unittest discover -s ./tests -p "*_test.py"
-coverage report -m --omit='./tests/*','./examples/*','./docs/*','setup.py' 
-coverage html --omit='./tests/*','./examples/*','./docs/*','setup.py' -d htmlcov 
+coverage run --source=hubit -m unittest discover -s ./tests -p "*_test.py"
+coverage report -m  
+coverage html -d htmlcov 
 black hubit --check
 cd tests
 read -n 1 -s -r -p "Press any key to continue"

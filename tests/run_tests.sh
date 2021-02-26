56eccbccc3377045f0f61924315350ef941ee120
Jacob Sonne <mrsonne@gmail.com>
2021-02-26 22:04:56 +0100

    check all with black

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -2,6 +2,6 @@ cd ..
 coverage run --source=hubit -m unittest discover -s ./tests -p "*_test.py"
 coverage report -m  
 coverage html -d htmlcov 
-black hubit --check
+black --check .
 cd tests
 read -n 1 -s -r -p "Press any key to continue"

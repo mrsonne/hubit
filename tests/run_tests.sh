a028ee736625da6873f655e4dc25a03073e4d505
Jacob Sonne <mrsonne@gmail.com>
2021-02-27 22:48:28 +0100

    add minimal mypy run

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -3,5 +3,6 @@ coverage run --source=hubit -m unittest discover -s ./tests -p "*_test.py"
 coverage report -m  
 coverage html -d htmlcov 
 black --check .
+mypy hubit/model.py
 cd tests
 read -n 1 -s -r -p "Press any key to continue"

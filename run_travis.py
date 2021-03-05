9b6e02d014dddf40bd79cf11787220131e38f968
Jacob Sonne <mrsonne@gmail.com>
2021-03-05 20:29:41 +0100

    run travis script steps locally

---
+++ run_travis.py
@@ -0,0 +1,13 @@
+import subprocess
+import yaml
+
+with open(".travis.yml", "r") as stream:
+    steps = yaml.load(stream)["script"]
+
+extra_steps = "coverage html -d htmlcov",
+steps.extend(extra_steps)
+
+nsteps = len(steps)
+for istep, step in enumerate(steps, 1):
+    print(f'Running step {istep} of {nsteps}: {step}' )
+    subprocess.run(step, shell=True)

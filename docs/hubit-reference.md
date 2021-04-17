be65562979a3cbbfef6e0b616482c8f00d3b62d0
Jacob Sonne <mrsonne@gmail.com>
2021-04-17 23:07:05 +0200

    add package-level docs

---
+++ docs/hubit-reference.md
@@ -0,0 +1,10 @@
+
+{% include 'shared/header.html' %}
+
+# Hubit
+
+<h2 id="hubit.model" class="doc doc-heading">
+    <code>hubit.clear_hubit_cache()</code>
+</h2>
+::: hubit.__init__.clear_hubit_cache
+
--- hubit/__init__.py
+++ hubit/__init__.py
@@ -1,11 +1,13 @@
 VERSION = "0.3.0a"
-
 import shutil
-import os
+
 
 # Make HubitModel available as from hubit import model
 from .model import HubitModel, _CACHE_DIR
 
-
 def clear_hubit_cache():
+    """
+    Clear the cache for all models. Will delete all serialized model cache
+    from the disk.
+    """
     shutil.rmtree(_CACHE_DIR, ignore_errors=True)

e88d10d8c97bc3691c3a5a50fe4ef59b9b6a6a33
Jacob Sonne <mrsonne@gmail.com>
2021-04-28 07:17:08 +0200

    black

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from hubit import VERSION
 
-def define_env(env):
-  "Hook function for mkdocs"
 
-  @env.macro
-  def get_version():
-      return VERSION
+def define_env(env):
+    "Hook function for mkdocs"
+
+    @env.macro
+    def get_version():
+        return VERSION

9a0c3dde2b7c39d4c63b5b57e1ad39ef0b51b04a
Jacob Sonne <mrsonne@gmail.com>
2021-05-09 20:19:57 +0200

    black

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
-# Required for Github actions to work... 
+# Required for Github actions to work...
 import sys
 import pathlib
-THIS_FILE_DIR =  pathlib.Path(__file__).parent.absolute()
+
+THIS_FILE_DIR = pathlib.Path(__file__).parent.absolute()
 sys.path.insert(0, THIS_FILE_DIR.as_posix())
 sys.path.insert(0, THIS_FILE_DIR.joinpath("hubit").as_posix())
 
 from hubit import VERSION
+
 
 def define_env(env):
     "Hook function for mkdocs"

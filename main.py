d2ea8eaa4a7479d5c516bd418ef38011ae5d4aee
Jacob Sonne <mrsonne@gmail.com>
2021-05-09 20:11:35 +0200

    hold nu op!

--- main.py
+++ main.py
@@ -1,14 +1,22 @@
-import os
+# import os
+# import sys
+# WORK_DIR = os.getcwd()
+# THIS_FILE_DIR = os.path.dirname(os.path.abspath(__file__))
+# sys.path.insert(0, THIS_FILE_DIR)
+# sys.path.insert(0, os.path.join(THIS_FILE_DIR, "hubit"))
+# print(sys.path)
+
 import sys
 import pathlib
 THIS_FILE_DIR =  pathlib.Path(__file__).parent.absolute()
 print(THIS_FILE_DIR)
-WORK_DIR = os.getcwd()
-# sys.path.insert(0, THIS_FILE_DIR)
+sys.path.insert(0, THIS_FILE_DIR)
 sys.path.insert(0, THIS_FILE_DIR.joinpath("hubit"))
-# print(sys.path)
+print(sys.path)
+
 
 from hubit import VERSION
+# import setup.version as version
 
 def define_env(env):
     "Hook function for mkdocs"

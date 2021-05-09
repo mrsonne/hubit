5756077563a33068f7a6444f1fec77126e3f6904
Jacob Sonne <mrsonne@gmail.com>
2021-05-09 20:08:37 +0200

    arghh

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
+import os
 import sys
 import pathlib
 THIS_FILE_DIR =  pathlib.Path(__file__).parent.absolute()
 print(THIS_FILE_DIR)
-# WORK_DIR = os.getcwd()
-# THIS_FILE_DIR = os.path.dirname(os.path.abspath(__file__))
-sys.path.insert(0, THIS_FILE_DIR)
-# sys.path.insert(0, os.path.join(THIS_FILE_DIR, "hubit"))
+WORK_DIR = os.getcwd()
+# sys.path.insert(0, THIS_FILE_DIR)
+sys.path.insert(0, THIS_FILE_DIR.joinpath("hubit"))
 # print(sys.path)
 
 from hubit import VERSION

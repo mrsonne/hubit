a2a3839a456430117bd5210fd233732a993e2952
Jacob Sonne <mrsonne@gmail.com>
2021-05-15 22:40:40 +0200

    add hard referenced indices

--- examples/flow/components/mod1.py
+++ examples/flow/components/mod1.py
@@ -2,6 +2,7 @@ from time import sleep
 
 
 def main(_input_consumed, _results_consumed, results_provided):
+    print(_input_consumed)
     results_provided["inflow"] = (
         _input_consumed["total_flow"]
         * _input_consumed["fraction"]
--- examples/flow/run.py
+++ examples/flow/run.py
@@ -3,6 +3,12 @@ from .shared import get_model
 
 # logging.basicConfig(level=logging.INFO)
 hmodel = get_model("model1.yml")
-query = ["inlets[:].tanks[2].inflow"]
+# query = ["inlets[:].tanks[2].inflow"]
+
+# For hard refs
+# query = ["inlets[0].tanks[2].inflow"]
+query = ["inlets[1].tanks[2].inflow"]
+# query = ["inlets[1].tanks[1].inflow"]
+
 response = hmodel.get(query, use_multi_processing=False)
 print(response)

67527f61741a0c20c9b6fda97afd54b95c16cae6
Jacob Sonne <mrsonne@gmail.com>
2021-05-15 06:20:16 +0200

    first version of flow that works

--- examples/flow/components/mod1.py
+++ examples/flow/components/mod1.py
@@ -3,7 +3,9 @@ from time import sleep
 
 def main(_input_consumed, _results_consumed, results_provided):
     results_provided["inflow"] = (
-        _input_consumed["total_flow"] * _input_consumed["fraction"]
+        _input_consumed["total_flow"]
+        * _input_consumed["fraction"]
+        * _input_consumed["number"]
     )
 
 
--- examples/flow/run.py
+++ examples/flow/run.py
@@ -2,12 +2,7 @@ from pprint import pprint
 from .shared import get_model
 
 # logging.basicConfig(level=logging.INFO)
-
 hmodel = get_model("model1.yml")
-query = ["inlets[0].tanks[2].inflow"]
+query = ["inlets[:].tanks[2].inflow"]
 response = hmodel.get(query, use_multi_processing=False)
 print(response)
-
-# query = ["tanks[1].inflow"]
-# response = hmodel.get(query, use_multi_processing=False)
-# print(response)

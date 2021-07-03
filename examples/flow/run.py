6e4b12bb3c3e358957f96df3e3da3f2929e7619d
Jacob Sonne <mrsonne@gmail.com>
2021-07-03 08:26:15 +0200

    add comments

--- examples/flow/run.py
+++ examples/flow/run.py
@@ -4,16 +4,28 @@ from .shared import get_model
 # logging.basicConfig(level=logging.INFO)
 # For hard refs
 hmodel = get_model("model1.yml")
+
+# Spawns 1 worker
 # query = ["tanks[0].vol_outlet_flow"]
+
+# Spawns 2 workers
 # query = ["tanks[1].vol_outlet_flow"]
-query = ["tanks[2].vol_outlet_flow"]
+
+# Spawns 3 workers
+# query = ["tanks[2].vol_outlet_flow"]
+
+
+# Spawns 3 workers
 # query = [
 #     "tanks[0].vol_outlet_flow",
 #     "tanks[1].vol_outlet_flow",
 #     "tanks[2].vol_outlet_flow",
 # ]
 
-# Fatal error. Multiple providers for query 'tanks[:].vol_outlet_flow': ['cmp0@./components/mod1.main', 'cmp1@./components/mod1.main']
-# query = ["tanks[:].vol_outlet_flow"]
+# Spawns 3 workers
+query = ["tanks[:].vol_outlet_flow"]
+
 response = hmodel.get(query, use_multi_processing=False)
+
 print(response)
+print(hmodel.log())

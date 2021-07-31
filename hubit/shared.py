d3ebbe3a679a1904ce3f43c30d6b0886b7159610
Jacob Sonne <mrsonne@gmail.com>
2021-07-31 20:18:40 +0200

    improve print length tree

--- hubit/shared.py
+++ hubit/shared.py
@@ -563,7 +563,7 @@ class LengthTree:
                 f"nodes={len(nodes)}, "
                 f"parents={nparents}, "
                 f"children={nchildren}, "
-                f"children_is_none (children are leafs)={children_is_none}"
+                f"children are leaves={children_is_none}"
             )
 
         lines.append("--------------------")

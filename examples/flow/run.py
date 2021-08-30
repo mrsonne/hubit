0598ea1dc5d30e1ef7d0aeb93445db8cb5e2e296
Jacob Sonne <mrsonne@gmail.com>
2021-08-30 19:00:47 +0200

    improved printing & rename lines to sites

--- examples/flow/run.py
+++ examples/flow/run.py
@@ -5,47 +5,75 @@ from .shared import get_model
 # For hard refs
 hmodel = get_model("model1.yml")
 
-query = ["lines[0].tanks[0].vol_outlet_flow"]
-print(f"\nSpawns 1 worker: {query}")
+# Should result in a number
+query = ["sites[0].lines[0].tanks[0].vol_outlet_flow"]
+print(f"\nQuery")
+pprint(query)
+print(f"Spawns 1 worker")
 response = hmodel.get(query, use_multi_processing=False)
-print("response", response)
+print("Response (one path & value):")
+pprint(response)
 print(hmodel.log())
 hmodel.clean_log()
 
-query = ["lines[0].tanks[1].vol_outlet_flow"]
-print(f"\nSpawns 2 workers: {query}")
+# Should result in a number
+query = ["sites[0].lines[0].tanks[1].vol_outlet_flow"]
+print(f"\nQuery")
+pprint(query)
+print(f"Spawns 2 workers")
 response = hmodel.get(query, use_multi_processing=False)
-print("response", response)
+print("Response (one path & value):")
+pprint(response)
 print(hmodel.log())
 hmodel.clean_log()
 
-query = ["lines[0].tanks[2].vol_outlet_flow"]
-print(f"\nSpawns 3 workers: {query}")
+# # Should result in a number
+print(f"\nQuery")
+query = ["sites[0].lines[0].tanks[2].vol_outlet_flow"]
+pprint(query)
+print(f"Spawns 3 workers")
 response = hmodel.get(query, use_multi_processing=False)
-print("response", response)
+print("Response (one path & value):")
+pprint(response)
 print(hmodel.log())
 hmodel.clean_log()
 
+# Should result in three numbers
 query = [
-    "lines[0].tanks[0].vol_outlet_flow",
-    "lines[0].tanks[1].vol_outlet_flow",
-    "lines[0].tanks[2].vol_outlet_flow",
+    "sites[0].lines[0].tanks[0].vol_outlet_flow",
+    "sites[0].lines[0].tanks[1].vol_outlet_flow",
+    "sites[0].lines[0].tanks[2].vol_outlet_flow",
 ]
-print(f"\nSpawns 3 workers: {query}")
+print(f"\nQuery")
+pprint(query)
+print(f"Spawns 3 workers")
 response = hmodel.get(query, use_multi_processing=False)
-print("response", response)
+print("Response (three paths & corresponding values)")
+pprint(response)
 print(hmodel.log())
 hmodel.clean_log()
 
-query = ["lines[0].tanks[:].vol_outlet_flow"]
+# Should result in 1D array
+query = ["sites[0].lines[0].tanks[:].vol_outlet_flow"]
 print(f"\nSpawns 3 workers: {query}")
 response = hmodel.get(query, use_multi_processing=False)
-print("response", response)
+print("Response (One path with list as value):")
+print(response)
 print(hmodel.log())
 
-
-# query = ["lines[:].tanks[:].vol_outlet_flow"]
+# Should result in double nested list
+# query = ["sites[0].lines[:].tanks[:].vol_outlet_flow"]
 # print(f"\nSpawns 3 workers: {query}")
 # response = hmodel.get(query, use_multi_processing=False)
-# print("response", response)
+# print("Response (One path with double nested list as value):")
+# print(response)
+# print(hmodel.log())
+
+
+# Should result in triple nested list
+# query = ["sites[:].lines[:].tanks[:].vol_outlet_flow"]
+# print(f"\nSpawns 3 workers: {query}")
+# response = hmodel.get(query, use_multi_processing=False)
+# print("Response (One path with triple nested list as value):")
+# print(response)
 # print(hmodel.log())

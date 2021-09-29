56a9024411c29442bfdb245a9b3ab6376ba55884
Jacob Sonne <mrsonne@gmail.com>
2021-09-29 10:48:28 +0200

    add typing

--- examples/flow/components/mod1.py
+++ examples/flow/components/mod1.py
@@ -1,7 +1,12 @@
-from time import sleep
+from __future__ import annotations
+from typing import TYPE_CHECKING, Dict
+
+if TYPE_CHECKING:
+    from hubit.utils import ReadOnlyDict
 
 
-def main(_input_consumed, results_provided):
+def main(_input_consumed: ReadOnlyDict, results_provided: Dict):
+
     tank_parameters = _input_consumed["tank_parameters"]
     vol_inlet_flow = _input_consumed["vol_inlet_flow"]
 
@@ -11,5 +16,5 @@ def main(_input_consumed, results_provided):
     results_provided["vol_over_flow"] = 12.0
 
 
-def version():
+def version() -> str:
     return "1.0.0"

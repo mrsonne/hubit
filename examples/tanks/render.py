cf1e0272a21e04a4d18402d59f6a22872f08afd7
Jacob Sonne <mrsonne@gmail.com>
2021-10-05 13:47:28 +0200

    change query to illustrate a reduced call graph

--- examples/tanks/render.py
+++ examples/tanks/render.py
@@ -4,10 +4,7 @@ models = (
     "model_1.yml",
     # "model_2.yml",
 )
-query = (
-    "prod_sites[0].prod_lines[0].tanks[2].Q_yield",
-    "prod_sites[0].prod_lines[:].tanks[:].Q_yield",
-)
+query = ("prod_sites[0].prod_lines[0].tanks[1].Q_yield",)
 for _id, model in enumerate(models, 1):
     hmodel = get_model(model, model_id=_id)
     hmodel.render()

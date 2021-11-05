1068e284009c01c4150a9b5b7521bcf5317f6ff3
Jacob Sonne <mrsonne@gmail.com>
2021-11-05 04:23:33 +0100

    facelift

--- run_travis.py
+++ run_travis.py
@@ -1,14 +1,17 @@
 #!/usr/bin/python3
 import subprocess
 import yaml
+import logging
+
+logging.basicConfig(level=logging.INFO)
 
 with open(".travis.yml", "r") as stream:
-    steps = yaml.load(stream)["script"]
+    steps = yaml.load(stream, Loader=yaml.SafeLoader)["script"]
 
 extra_steps = ("coverage html -d htmlcov",)
 steps.extend(extra_steps)
 
 nsteps = len(steps)
 for istep, step in enumerate(steps, 1):
-    print(f"Running step {istep} of {nsteps}: {step}")
+    logging.info(f"Running step {istep} of {nsteps}: {step}")
     subprocess.run(step, shell=True)

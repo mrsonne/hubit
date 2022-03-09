45efec1c284fd8bbf16afa02b208f098c588c767
Jacob Sonne <mrsonne@gmail.com>
2022-03-09 21:01:52 +0100

    print some info

--- examples/tanks/components/unit_price.py
+++ examples/tanks/components/unit_price.py
@@ -17,13 +17,17 @@ def main(_input_consumed: ReadOnlyDict, results_provided: Dict):
     """
     timeout = urllib3.Timeout(connect=1.0, read=5.0)
     url = _input_consumed["url"]
-    http = urllib3.PoolManager(timeout=timeout)
-    headers = {"Cache-Control": "no-cache", "Pragma": "no-cache"}
-    response = http.request("GET", url, headers=headers)
-    # print(response.headers)
+    headers = {
+        "cache-control": "no-cache",
+        "pragma": "no-cache",
+    }
+    http = urllib3.PoolManager(timeout=timeout, headers=headers)
+    print(f"Looking up price at '{url}'")
+    response = http.request("GET", url)
     if not (response.status == 200):
         raise HubitError(f"Could not connect to '{url}'")
     payload = json.loads(response.data.decode("utf-8"))
+    print(f"Received price '{payload['unit_price']}'")
     results_provided["unit_price"] = payload["unit_price"]
 
 

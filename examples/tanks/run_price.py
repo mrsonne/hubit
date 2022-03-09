98ef770c267a65fa86ce773edc5fc8ac8a4e5a94
Jacob Sonne <mrsonne@gmail.com>
2022-03-09 21:02:51 +0100

    split out price calc

---
+++ examples/tanks/run_price.py
@@ -0,0 +1,26 @@
+import logging
+from pprint import pprint
+from .shared import get_model
+
+# logging.basicConfig(level=logging.INFO)
+
+
+def run_sales_calc(model_id, input_file="input.yml"):
+    use_multi_processing = True
+    hmodel = get_model(model_id, input_file)
+    response = hmodel.get(
+        [
+            "prod_sites[:].prod_lines[:].revenue",
+        ],
+        use_multi_processing=use_multi_processing,
+    )
+    print("\nModel results")
+    pprint(hmodel.results)
+
+    print("\nModel response")
+    print(response)
+
+
+if __name__ == "__main__":
+    input_file = "input.yml"
+    run_sales_calc("model_1.yml", input_file)

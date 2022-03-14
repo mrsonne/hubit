d1e64d888a2d62b15deaabf0e5680d9c6994ec57
Jacob Sonne <mrsonne@gmail.com>
2022-03-14 12:50:46 +0100

    minimum temperature in the service layer

---
+++ examples/wall/components/min_temperature.py
@@ -0,0 +1,32 @@
+from __future__ import annotations
+from typing import TYPE_CHECKING, Dict
+
+if TYPE_CHECKING:
+    from hubit.utils import ReadOnlyDict
+
+
+def main(_input_consumed: ReadOnlyDict, results_provided: Dict):
+    """
+    Find minimum temperature in the service layer excluding segments with a type
+    in "no_service_segment_types". The segment types that are
+    excluded is set in the input rendering the implementation more stable.
+
+    The definition of the service layer as being the interface between the
+    outermost and second outermost wall layer is controlled by the model and
+    the component is unaware of the choice.
+    """
+    service_layer_min_temps = _input_consumed["service_layer_minimum_temperatures"]
+    segment_types = _input_consumed["segment_types"]
+    no_service_segment_types = _input_consumed["no_service_segment_types"]
+
+    service_layer_min_temp = min(
+        min_temp
+        for segment_type, min_temp in zip(segment_types, service_layer_min_temps)
+        if not (segment_type in no_service_segment_types)
+    )
+
+    results_provided["service_layer_minimum_temperature"] = service_layer_min_temp
+
+
+def version():
+    return 1.0
---
+++ examples/wall/run_min_temperature.py
@@ -0,0 +1,20 @@
+import logging
+
+from hubit.model import HubitModel
+from .utils import get_model
+
+logging.basicConfig(level=logging.INFO)
+
+
+def run_query(hmodel: HubitModel, use_multi_processing: bool):
+    response = hmodel.get(
+        ["service_layer_minimum_temperature"],
+        use_multi_processing=use_multiprocessing,
+    )
+    print(response)
+
+
+if __name__ == "__main__":
+    hmodel = get_model()
+    use_multiprocessing = True
+    run_query(hmodel, use_multi_processing=use_multiprocessing)

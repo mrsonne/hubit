cc86ab9c776ab2335c4b494366fac95d1dc5e00a
Jacob Sonne <mrsonne@gmail.com>
2022-04-09 22:13:19 +0200

    more docs

--- examples/space-time/eq_non_competitive.py
+++ examples/space-time/eq_non_competitive.py
@@ -2,32 +2,40 @@ from typing import Dict
 from hubit.utils import ReadOnlyDict
 
 
-def main(_input_consumed: ReadOnlyDict, results_provided: Dict):
+def main(_input: ReadOnlyDict, results_provided: Dict):
     """
     Finds the equilibrium for the dissolution of A from a liquid (l) into a solid
     (s) phase.
 
     A(l) ⇌ A(s)
 
+    At equilibrium the following relation is fulfilled
+
     c_s / c_l = k_eq
 
+    where c_s and c_l are the concentrations of A in the solid and
+    liquid phases, respectively. When the total amount of A is n the
+    equilibrium can be illustrated as
+
                A(l)        ⇌       A(s)
-    start   n_tot                   0
-    eq      n_tot - x               x
+    start       n                   0
+    eq        n - x                 x
 
-    Inserting into the equilibrium relation gives
+    When the volume of the solid and liquid phase are V_s and V_l, respectively,
+    the equilibrium relation may be written
 
-    x / (n_tot - x) = k_eq * V_s / V_l
+    x / (n - x) = k_eq * V_s / V_l.
 
-    and therefore
+    Therefore, x may be written
 
-    x = (n_tot * k_eq * V_s / V_l) / (1 + k_eq * V_s / V_l)
+    x = (n * k_eq * V_s / V_l) / (1 + k_eq * V_s / V_l)
 
-    For multiple components A each equilibrium is assumed to be independent.
+    For multiple components, each equilibrium is assumed to be independent.
     """
-    tot_composition = _input_consumed["tot_composition"]
-    k_eq = _input_consumed["cell"]["k_eq"]
-    V_solid = _input_consumed["cell"]["V_solid"]
+    n = _input["mole_numbers"]
+    k_eq = _input["cell"]["k_eq"]
+    V_solid = _input["cell"]["V_solid"]
+    V_liq = _input["V_liq"]
 
     results_provided["liq_composition"] = 1
     results_provided["sol_composition"] = 1

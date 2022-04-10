a282391e85c5e01d60b0855cb15b115ba69b994d
Jacob Sonne <mrsonne@gmail.com>
2022-04-10 11:54:35 +0200

    add implementation

--- examples/space-time/components/mix.py
+++ examples/space-time/components/mix.py
@@ -2,5 +2,7 @@ from typing import Dict
 from hubit.utils import ReadOnlyDict
 
 
-def main(_input_consumed: ReadOnlyDict, results_provided: Dict):
-    pass
+def main(_input: ReadOnlyDict, results: Dict):
+    liq_concs = _input["liq_concs"]
+    V_liq = _input["V_liq"]
+    results["mole_numbers"] = [conc * V_liq for conc in liq_concs]

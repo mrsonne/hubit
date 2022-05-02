8ab8d4c076f0de19071fef799146a7d326c2de80
Jacob Sonne <mrsonne@gmail.com>
2022-05-02 15:48:58 +0200

    advection example

---
+++ examples/advection/README.md
@@ -0,0 +1,8 @@
+## Advection example?!?
+
+constant flow in x v_x
+du/dt + v_x*du/dx = 0
+
+( u(n, k) - u(n-1, k) ) / delta t + v*( u(n, k) - u(n, k-1) ) delta x = 0
+
+( u(n, k) - u(n-1, k) )   + v*( u(n, k) - u(n, k-1) ) delta t / delta x = 0
---
+++ examples/advection/run.py
@@ -0,0 +1,66 @@
+import pathlib
+import pprint
+import yaml
+import os
+from hubit.model import HubitModel
+
+THIS_DIR = pathlib.Path(__file__).parent
+TMP_DIR = THIS_DIR.joinpath("tmp")
+pathlib.Path(TMP_DIR).mkdir(parents=True, exist_ok=True)
+
+
+def make_input():
+    """Take the original input and create input object more
+    suitable for Hubit
+
+    The input specifies the number of (identical) cells and the number
+    of batches we want to run through the cells as well as the parameters
+    for the cells.
+    """
+    # Load the input
+    with open(THIS_DIR.joinpath("input.yml"), "r") as stream:
+        input_data = yaml.load(stream, Loader=yaml.FullLoader)
+
+    # Boundary condition all cells at t = 0
+    feed_concs = [0.0 for _ in input_data["feed"]["concs"]]
+
+    # Create all cells
+    input_ = {
+        "batches": [
+            {
+                "cells": [{**input_data["cell"], "ini": {"concs": feed_concs, "V": 0}}]
+                * input_data["calc"]["n_cells"]
+            }
+        ]
+        * input_data["calc"]["n_batches"]
+    }
+
+    for batch in input_["batches"]:
+        batch["cells"][0]["ini"]["concs"] = input_data["feed"]["concs"]
+        batch["cells"][0]["ini"]["V"] = input_data["feed"]["V"]
+
+    # Save input (not necessary)
+    with open(TMP_DIR.joinpath("input_expanded.yml"), "w") as handle:
+        yaml.dump(input_, handle)
+
+    return input_
+
+
+def run(inp):
+    # Load the model
+    # with open(THIS_DIR.joinpath("model2.yml"), "r") as stream:
+    #     model_cfg = yaml.load(stream, Loader=yaml.FullLoader)
+    # pprint.pprint(model_cfg)
+
+    model = HubitModel.from_file(THIS_DIR.joinpath("model2.yml"))
+    model.set_input(inp)
+
+    # qpaths = ["time[:].position[0].u"]
+    # response = model.get(qpaths)
+    # print(response)
+
+
+if __name__ == "__main__":
+    inp = make_input()
+    # pprint.pprint(inp)
+    # run(inp)

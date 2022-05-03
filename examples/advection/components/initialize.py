8904d9d5b527f67feb8dd8ef996f856c881cfe87
Jacob Sonne <mrsonne@gmail.com>
2022-05-03 12:30:30 +0200

    first commit

---
+++ examples/advection/components/initialize.py
@@ -0,0 +1,12 @@
+# provides_results:
+#   - name: u
+#     path: time[IDX_TIME].position[IDX_POS].u
+#   - name: t
+#     path: time[IDX_TIME].position[IDX_POS].t
+# consumes_input:
+#   - name: dummy
+#     path: time[IDX_TIME].position[IDX_POS].dummy
+#   - name: u
+#     path: init.u
+#   - name: t
+#     path: init.time

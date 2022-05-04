54d4788c3d2581f96451f809a8b455c0d5766495
Jacob Sonne <mrsonne@gmail.com>
2022-05-04 22:42:59 +0200

    use tend not n*delta_t

--- examples/advection/run.py
+++ examples/advection/run.py
@@ -10,6 +10,10 @@ TMP_DIR = THIS_DIR.joinpath("tmp")
 pathlib.Path(TMP_DIR).mkdir(parents=True, exist_ok=True)
 
 
+def n_timesteps(input_data):
+    return int(input_data["calc"]["t_end"] / input_data["calc"]["delta_t"]) + 1
+
+
 def make_input():
     """Take the original input and create input object more
     suitable for Hubit
@@ -23,6 +27,7 @@ def make_input():
         input_data = yaml.load(stream, Loader=yaml.FullLoader)
 
     # Boundary condition all cells at t = 0
+    n_times = n_timesteps(input_data)
     input_ = {
         "time": [
             {
@@ -31,7 +36,7 @@ def make_input():
                     for idx_pos in range(input_data["init"]["n_positions"])
                 ]
             }
-            for idx_time in range(input_data["init"]["n_times"])
+            for idx_time in range(n_times)
         ]
     }
 
@@ -62,7 +67,8 @@ def run(inp):
     print(response)
 
     # TODO: : and -1 doest work yet
-    idx_time_max = inp["init"]["n_times"] - 1
+    n_times = n_timesteps(inp)
+    idx_time_max = n_times - 1
     idx_pos_max = inp["init"]["n_positions"] - 1
 
     qpaths = [f"time[{idx_time_max}].position[0].u"]
@@ -70,13 +76,14 @@ def run(inp):
     print(response)
 
     fig, axs = plt.subplots(2, 1, figsize=(12, 8))
+
     # Collect data from inlet
     values = [
         (
             model.results[f"time[{idx_time}].position[0].t"],
             model.results[f"time[{idx_time}].position[0].u"],
         )
-        for idx_time in range(inp["init"]["n_times"])
+        for idx_time in range(n_times)
     ]
 
     # Plot data from inlet
